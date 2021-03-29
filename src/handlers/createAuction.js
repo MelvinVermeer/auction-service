50d392ebb2fdc19babbba8ad5fec3b08aa549b42
Melvin Vermeer <melvin.vermeer@gmail.com>
2021-03-29 23:07:38 +0200

    add middleware

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,5 +1,10 @@
 import { v4 as uuid } from "uuid";
 import AWS from "aws-sdk";
+import middy from "@middy/core";
+import httpJsonBodyParser from "@middy/http-json-body-parser";
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import httpErrorHandler from "@middy/http-error-handler";
+import createError from "http-errors";
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
@@ -14,17 +19,25 @@ async function createAuction(event, context) {
     createdAt: now.toISOString(),
   };
 
-  await dynamoDB
-    .put({
-      TableName: process.env.AUCTIONS_TABLE_NAME,
-      Item: auction,
-    })
-    .promise();
-
+  try {
+    await dynamoDB
+      .put({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Item: auction,
+      })
+      .promise();
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error)
+  }
+  
   return {
     statusCode: 201,
     body: JSON.stringify(auction),
   };
 }
 
-export const handler = createAuction;
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
