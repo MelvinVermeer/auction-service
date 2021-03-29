bebaa55fc92a439589439f9dc28b2d001607cc8a
Melvin Vermeer <melvin.vermeer@gmail.com>
2021-03-29 22:03:19 +0200

    add uuid

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,12 +1,17 @@
+import { v4 as uuid} from "uuid";
+
 async function createAuction(event, context) {
   const { title } = JSON.parse(event.body);
   const now = new Date();
 
   const auction = {
+    id: uuid(),
     title,
     status: "OPEN",
     createdAt: now.toISOString(),
   };
+
+  //AuctionsTable
 
   return {
     statusCode: 201,
