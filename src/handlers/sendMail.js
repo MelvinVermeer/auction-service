dbeaf8f90b6b2014e5816ba6977a3c1ccaaaebc8
Melvin Vermeer <melvin.vermeer@gmail.com>
2021-03-30 15:45:27 +0200

    sending an email from SES

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,8 +1,30 @@
-import commonMiddleware from "../lib/commonMiddleware";
+import AWS from "aws-sdk";
 
-async function sendMail(event) {
-  console.log(event);
-  return event;
+const ses = new AWS.SES({ region: "eu-west-1" });
+
+async function sendMail() {
+  const params = {
+    Source: "melvin.vermeer@gmail.com",
+    Destination: { ToAddresses: ["melvin.vermeer@gmail.com"] },
+    Message: {
+      Body: {
+        Text: {
+          Data: "Hi, from auction service",
+        },
+      },
+      Subject: {
+        Data: "Hi",
+      },
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    return result;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 }
 
-export const handler = commonMiddleware(sendMail);
+export const handler = sendMail;
