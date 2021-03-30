fad2758e9e19ef1d580ff81520433dd253867329
Melvin Vermeer <melvin.vermeer@gmail.com>
2021-03-30 13:15:39 +0200

    implement closing ended auctions

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,6 +1,16 @@
-async function processAuctions(event, context) {
-  console.log("processing auctions");
-  console.log({ event, context });
+import createError from "http-errors";
+import { closeAuction } from "../lib/closeAuction";
+import { getEndedAuctions } from "../lib/getEndedAuctions";
+
+async function processAuctions() {
+  try {
+    const auctions = await getEndedAuctions();
+    const closedAuctions = await Promise.all(auctions.map(closeAuction));
+
+    return { closed: closedAuctions.length };
+  } catch (error) {
+    throw new createError.InternalServerError(error);
+  }
 }
 
 export const handler = processAuctions;
