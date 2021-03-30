4fa54be2c3acc8d1521e75a48444be133e3bdca8
Melvin Vermeer <melvin.vermeer@gmail.com>
2021-03-30 13:32:40 +0200

    getAuctions validation

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,6 +1,8 @@
 import AWS from "aws-sdk";
 import createError from "http-errors";
+import validator from "@middy/validator";
 import commonMiddleware from "../lib/commonMiddleware";
+import getAuctionsSchema from "../lib/schemas/getAuctionsSchema";
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
@@ -29,4 +31,6 @@ async function getAuctions(event) {
   }
 }
 
-export const handler = commonMiddleware(getAuctions);
+export const handler = commonMiddleware(getAuctions).use(
+  validator({ inputSchema: getAuctionsSchema, useDefaults: true })
+);
