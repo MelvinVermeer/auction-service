3965d4f2bdaeead8655a2f03fae85505dc84f2ce
Melvin Vermeer <melvin.vermeer@gmail.com>
2021-03-30 17:16:33 +0200

    send emails when auction is closed

--- src/lib/closeAuction.js
+++ src/lib/closeAuction.js
@@ -1,6 +1,7 @@
 import AWS from "aws-sdk";
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const sqs = new AWS.SQS();
 
 export async function closeAuction(auction) {
   const params = {
@@ -15,7 +16,33 @@ export async function closeAuction(auction) {
     },
   };
 
-  const result = await dynamoDB.update(params).promise();
+  await dynamoDB.update(params).promise();
 
-  return result;
+  const { title, seller, highestBid } = auction;
+  const { amount, bidder } = highestBid;
+
+  console.log(process.env.MAILQUEUE_URL);
+
+  const notifySeller = sqs
+    .sendMessage({
+      QueueUrl: process.env.MAILQUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: "Your item has been sold!",
+        recipient: seller,
+        body: `Your item ${title} has been sold for ${amount}`,
+      }),
+    })
+    .promise();
+  const notifyBidder = sqs
+    .sendMessage({
+      QueueUrl: process.env.MAILQUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: "You are the highest bidder!",
+        recipient: bidder,
+        body: `You won the auction for ${title} for ${amount}`,
+      }),
+    })
+    .promise();
+
+  await Promise.all([notifyBidder, notifySeller]);
 }
