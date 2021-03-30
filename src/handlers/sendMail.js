296a282951a8e2fb9cbb4bcea6b45d903783b121
Melvin Vermeer <melvin.vermeer@gmail.com>
2021-03-30 16:25:50 +0200

    send mails from log

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -2,18 +2,23 @@ import AWS from "aws-sdk";
 
 const ses = new AWS.SES({ region: "eu-west-1" });
 
-async function sendMail() {
+async function sendMail(event) {
+  const record = event.Records[0];
+  console.log(record);
+
+  const { subject, recipient, body } = JSON.parse(record.body);
+
   const params = {
     Source: "melvin.vermeer@gmail.com",
-    Destination: { ToAddresses: ["melvin.vermeer@gmail.com"] },
+    Destination: { ToAddresses: [recipient] },
     Message: {
       Body: {
         Text: {
-          Data: "Hi, from auction service",
+          Data: body,
         },
       },
       Subject: {
-        Data: "Hi",
+        Data: subject,
       },
     },
   };
