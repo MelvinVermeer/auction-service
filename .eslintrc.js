8dc703df0f78f72a283709cf1a705b35e052cf69
Melvin Vermeer <melvin.vermeer@gmail.com>
2021-04-01 13:18:59 +0200

    add eslint plugin for jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ["airbnb-base", "plugin:prettier/recommended"],
-  plugins: ["prettier"],
+  plugins: ["prettier", "jest"],
   root: true,
   parserOptions: {
     ecmaVersion: 2018,
@@ -9,5 +9,8 @@ module.exports = {
     "prettier/prettier": "error",
     "import/prefer-default-export": "off",
     "no-console": "off",
+  },
+  env: {
+    "jest/globals": true,
   },
 };
